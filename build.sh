683ebd6d1dcdb596c2ccafe798d53f3ebb5c4cdb
taoneswa kasirai <61433155+taoneswa@users.noreply.github.com>
2025-04-08 13:23:58 +0200

    render

--- build.sh
+++ build.sh
@@ -1,14 +1,20 @@
+#!/usr/bin/env bash
 set -o errexit
 set -o pipefail
 set -o nounset
 
-
+echo "Installing dependencies..."
 pip install -r requirements.txt
 
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+echo "Running database migrations..."
 python manage.py migrate
 
 if [[ $CREATE_SUPERUSER == "true" ]]; then
-python manage.py createsuperuser --noinput --email $DJANGO_SUPERUSER_EMAIL
+    echo "Creating superuser..."
+    python manage.py createsuperuser --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL
 fi
